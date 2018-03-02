c9be45800c25b7abec6bab275c973ade54a207fa
Paweł Botuliński <pawel.botulinski@gmail.com>
2018-03-02 22:04:04 +0100

    Pliki z laptopa

---
+++ Checkio/elementary/4-second-index.py
@@ -0,0 +1,29 @@
+def second_index(text: str, symbol: str):
+    """
+        returns the second index of a symbol in a given text
+    """
+    # your code here
+    bylem = False
+    x = 0
+    for i in text:
+        if i == symbol:
+            if bylem == True:
+                return x
+            bylem = True
+        x = x+1
+
+    return None
+
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(second_index("sims", "s"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert second_index("sims", "s") == 3, "First"
+    assert second_index("find the river", "e") == 12, "Second"
+    assert second_index("hi", " ") is None, "Third"
+    assert second_index("hi mayor", " ") is None, "Fourth"
+    assert second_index("hi mr Mayor", " ") == 5, "Fifth"
+    print('You are awesome! All tests are done! Go Check it!')
